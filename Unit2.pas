2324afacb1be73e4358310051b6a8846cb9ea6de
GostevaTatyana <ta_nysha97@mail.ru>
2016-05-29 22:58:17 +0700

    Add files via upload

--- Unit2.pas
+++ Unit2.pas
@@ -1,54 +1,28 @@
 unit Unit2;
 
+{$WARN SYMBOL_PLATFORM OFF}
+
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, MyObject_TLB, comobj;
+  Windows, ActiveX, Classes, ComObj, MyObject_TLB, StdVcl;
 
 type
-  TForm2 = class(TForm)
-    Edit1: TEdit;
-    Edit2: TEdit;
-    Edit3: TEdit;
-    Edit4: TEdit;
-    Edit5: TEdit;
-    Edit6: TEdit;
-    Button1: TButton;
-    ListBox1: TListBox;
-    procedure Button1Click(Sender: TObject);
-  private
-    { Private declarations }
-  public
-    { Public declarations }
+  TMyCom = class(TTypedComObject, IMyCom)
+  protected
+    function raschet(a, b, c, x: Single): Single; stdcall;
   end;
 
-var
-  Form2: TForm2;
 implementation
 
-{$R *.dfm}
+uses ComServ;
 
-procedure TForm2.Button1Click(Sender: TObject);
-var com:IMyCom;
-    xn,xk,dx,y,a,b,c,x:Extended;
-    i,n:Integer;
+function TMyCom.raschet(a, b, c, x: Single): Single;
 begin
-a:=StrToFloat(Edit1.Text);
-b:=StrToFloat(Edit2.Text);
-c:=StrToFloat(Edit3.Text);
-xn:=StrToFloat(Edit4.Text);
-xk:=StrToFloat(Edit5.Text);
-n:=StrToInt(Edit6.Text);
-dx:=(xk-xn)/n;
-x:=xn;
-com := CreateComObject(CLASS_MyCom) as IMyCom;
-for I := 0 to n-1 do
-  begin
-    y:=com.raschet(a,b,c,x);
-    ListBox1.Items.Add(FloatToStr(y));
-    x:=x+dx;
-  end;
+  Result:=a*sqr(x)+b*x+c;
 end;
 
+initialization
+  TTypedComObjectFactory.Create(ComServer, TMyCom, Class_MyCom,
+    ciMultiInstance, tmApartment);
 end.
