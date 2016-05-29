24019416a344db1361216635b663fb8c7b93683c
GostevaTatyana <ta_nysha97@mail.ru>
2016-05-29 22:59:27 +0700

    Add files via upload

---
+++ COMOb.pas
@@ -0,0 +1,60 @@
+unit COMOb;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, MyObject_TLB, comobj;
+
+type
+  TForm2 = class(TForm)
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    Edit5: TEdit;
+    Edit6: TEdit;
+    Button1: TButton;
+    ListBox1: TListBox;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+implementation
+
+{$R *.dfm}
+
+procedure TForm2.Button1Click(Sender: TObject);
+var com:IMyCom;
+    xn,xk,dx,y,a,b,c,x:Extended;
+    i,n:Integer;
+begin
+a:=StrToFloat(Edit1.Text);
+b:=StrToFloat(Edit2.Text);
+c:=StrToFloat(Edit3.Text);
+xn:=StrToFloat(Edit4.Text);
+xk:=StrToFloat(Edit5.Text);
+n:=StrToInt(Edit6.Text);
+dx:=(xk-xn)/n;
+x:=xn;
+com := CreateComObject(CLASS_MyCom) as IMyCom;
+for I := 0 to n-1 do
+  begin
+    y:=com.raschet(a,b,c,x);
+    ListBox1.Items.Add(FloatToStr(y));
+    x:=x+dx;
+  end;
+end;
+
+end.
